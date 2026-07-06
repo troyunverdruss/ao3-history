2bbf1621354e545b49afcc4fefec0f22160e4363
troy <troy@unverdruss.net>
2026-07-05 17:35:38 -0700

    small quotes

--- search.py
+++ search.py
@@ -5,8 +5,8 @@ def search(args):
     conn = connect(args.db)
     results = query(conn, args)
     for row in results:
-        print(f"{row["title"]} [ID: {row["work_id"]}]")
-        print(f"  By: {row["author"]}")
-        print(f"  Tags: {row["tags"]}")
-        print(f"  url: {row["url"]}")
+        print(f"{row['title']} [ID: {row['work_id']}]")
+        print(f"  By: {row['author']}")
+        print(f"  Tags: {row['tags']}")
+        print(f"  url: {row['url']}")
         print("")
